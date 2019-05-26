d7d6a5f3929b7a74efc491a2032bd778fcdc36f3
amity94 <45635119+amity94@users.noreply.github.com>
2019-05-26 20:20:05 +0300

    Add files via upload

---
+++ 2605lesson/2605lesson/DaysOfTheWeek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2605lesson
+{
+    public enum DaysOfTheWeek
+    {
+        Sunday = 1,
+        Monday,
+        Tuesday, 
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday 
+    }
+}
---
+++ 2605lesson/2605lesson/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2605lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            print(4);
+            Console.WriteLine("============");
+
+
+            printb(DaysOfTheWeek.Saturday);
+
+
+
+            Console.WriteLine("============");
+
+
+            Enum.GetNames(typeof(DaysOfTheWeek)).ToList().ForEach(s => Console.WriteLine(s));
+            Console.WriteLine("============");
+            DaysOfTheWeek[] days = (DaysOfTheWeek[])Enum.GetValues(typeof(DaysOfTheWeek));
+        }
+
+        public static void printb(DaysOfTheWeek day)
+        {
+            Console.WriteLine((int)day);
+
+
+            switch (day)
+            {
+                case DaysOfTheWeek.Sunday:
+                    Console.WriteLine("Rishon");
+                    break;
+                case DaysOfTheWeek.Monday:
+                    Console.WriteLine("Sheni");
+                    break;
+                case DaysOfTheWeek.Tuesday:
+                    Console.WriteLine("Slishi");
+                    break;
+                case DaysOfTheWeek.Wednesday:
+                    Console.WriteLine("Revii");
+                    break;
+                case DaysOfTheWeek.Thursday:
+                    Console.WriteLine("Hamishi");
+                    break;
+                case DaysOfTheWeek.Friday:
+                    Console.WriteLine("Shishi");
+                    break;
+                case DaysOfTheWeek.Saturday:
+                    Console.WriteLine("Shabat");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void print(int day)
+        {
+            Enum.TryParse($"{day}", out DaysOfTheWeek result);
+            Console.WriteLine(result);
+        }
+    }
+}
